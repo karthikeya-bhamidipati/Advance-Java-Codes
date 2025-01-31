b3900f7ecd5b5d816d2dfc4c646f7c93e4760ada
karthikeya-bhamidipati <karthikeyabh554@gmail.com>
2025-01-31 14:23:51 +0530

    in bankExample added all classes except the Bank class with all the fuctionalities

--- bankExample/bankExample.java
+++ bankExample/bankExample.java
@@ -1,28 +1,61 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 
-class bankExample{
-	static class BankAccount{
+class bankExample {
+	static class BankAccount {
 		int accountNumber;
 		double balance;
 		String accountHolderName;
-		void deposit(double amount){
+
+		void deposit(double amount) {
 			balance = balance + amount;
 		}
-		boolean withdraw(double amount){
-			if(balance - amount > 0){
+
+		boolean withdraw(double amount) {
+			if (balance - amount > 0) {
 				balance = balance - amount;
 				return true;
 			}
 			return false;
 		}
-		void displayAccountInfo(){
+
+		void displayAccountInfo() {
 			System.out.println("Name: " + accountHolderName);
-			System.out.println("Account number: "+ accountNumber);
+			System.out.println("Account number: " + accountNumber);
 			System.out.println("Balance: " + balance);
 		}
 	}
-	public static void main(String[] args){
-	
+
+	static class SavingsAccount extends BankAccount {
+		double intrestRate;
+
+		void addIntrest() {
+			double intrestAmount = 0;
+			intrestAmount = (balance * intrestRate * 30) / (100 * 365);
+			balance = balance + intrestAmount;
+		}
+	}
+
+	static class CurrentAccount extends BankAccount {
+		double overdraftLimit;
+
+		boolean withdraw(double amount) {
+			if (balance - amount < 0)
+				if (overdraftLimit - amount > 0) {
+					overdraftLimit = overdraftLimit - amount;
+					return true;
+				} else {
+					balance = balance - amount;
+					return true;
+				}
+			return false;
+		}
+	}
+
+	static class Bank {
+		ArrayList<BankAccount> accounts = new ArrayList<>();
+	}
+
+	public static void main(String[] args) {
+
 	}
 }
--- libraryExample/libraryExample.java
+++ libraryExample/libraryExample.java
@@ -2,103 +2,103 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class libraryExample {
- static class Book {
-    int bookId;
-    String title, author;
-    boolean isAvaliable;
+    static class Book {
+        int bookId;
+        String title, author;
+        boolean isAvaliable;
 
-    public Book() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("input format: '<bookId> <author> <title>'");
-        bookId = sc.nextInt();
-        author = sc.next();
-        title = sc.next();
-        sc.close();
-    }
-
-    public void displayBookInfo() {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
-        System.out.println("isAvaliable: " + isAvaliable);
-        System.out.println("\n\n");
-    }
-
-    public boolean borrowBook() {
-        if (isAvaliable) {
-            isAvaliable = false;
-            return true;
-
+        public Book() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("input format: '<bookId> <author> <title>'");
+            bookId = sc.nextInt();
+            author = sc.next();
+            title = sc.next();
+            sc.close();
         }
-        return false;
-    }
 
-    public void returnBook() {
-        isAvaliable = true;
-    }
-}
+        public void displayBookInfo() {
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("isAvaliable: " + isAvaliable);
+            System.out.println("\n\n");
+        }
 
- static class Library {
-    static ArrayList<Book> books = new ArrayList<>();
+        public boolean borrowBook() {
+            if (isAvaliable) {
+                isAvaliable = false;
+                return true;
 
-    public void addBook(Book book) {
-        books.add(book);
-    }
+            }
+            return false;
+        }
 
-    public void removeBook(int bookId) {
-        for (Book book : books) {
-            if (book.bookId == bookId)
-                books.remove(book);
+        public void returnBook() {
+            isAvaliable = true;
         }
     }
 
-    public void listAvaliableBooks() {
-        for (Book book : books) {
-            if (book.isAvaliable)
+    static class Library {
+        static ArrayList<Book> books = new ArrayList<>();
+
+        public void addBook(Book book) {
+            books.add(book);
+        }
+
+        public void removeBook(int bookId) {
+            for (Book book : books) {
+                if (book.bookId == bookId)
+                    books.remove(book);
+            }
+        }
+
+        public void listAvaliableBooks() {
+            for (Book book : books) {
+                if (book.isAvaliable)
+                    book.displayBookInfo();
+            }
+        }
+    }
+
+    static class Student {
+        int studentId;
+        String name;
+        ArrayList<Book> borrowedBooks = new ArrayList<>();
+
+        public Student() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("input format: '<studentId> <name>'");
+            studentId = sc.nextInt();
+            name = sc.next();
+            sc.close();
+        }
+
+        public void borrowBook(Book book) {
+            if (book.isAvaliable && borrowedBooks.size() <= 3) {
+                borrowedBooks.add(book);
+                book.borrowBook();
+            } else {
+                System.out.println("The book is not Avaliable! or You have reached your borrowing limit!");
+            }
+        }
+
+        public void returnBook(Book book) {
+            if (borrowedBooks.contains(book)) {
+                book.returnBook();
+            } else {
+                System.out.println("The book was not borrowed!");
+            }
+        }
+
+        public void displayBorrowedBooks() {
+            for (Book book : borrowedBooks) {
                 book.displayBookInfo();
-        }
-    }
-}
-
- static class Student {
-    int studentId;
-    String name;
-    ArrayList<Book> borrowedBooks = new ArrayList<>();
-
-    public Student() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("input format: '<studentId> <name>'");
-        studentId = sc.nextInt();
-        name = sc.next();
-        sc.close();
-    }
-
-    public void borrowBook(Book book) {
-        if (book.isAvaliable && borrowedBooks.size() <= 3) {
-            borrowedBooks.add(book);
-            book.borrowBook();
-        } else {
-            System.out.println("The book is not Avaliable! or You have reached your borrowing limit!");
+            }
         }
     }
 
-    public void returnBook(Book book) {
-        if (borrowedBooks.contains(book)) {
-            book.returnBook();
-        } else {
-            System.out.println("The book was not borrowed!");
-        }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Library saiReads = new Library();
+        System.out.println("Hello, World!");
     }
-
-    public void displayBorrowedBooks() {
-        for (Book book : borrowedBooks) {
-            book.displayBookInfo();
-        }
-    }
- }
-
-	public static void main(String[] args) {
-		Student s1 = new Student();
-		Library saiReads = new Library();
-		System.out.println("Hello, World!");
-	}
 }
