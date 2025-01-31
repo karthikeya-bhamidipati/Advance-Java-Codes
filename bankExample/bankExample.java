145d593f1f2585694fa47ad788b473a50129366e
karthikeya-bhamidipati <karthikeyabh554@gmail.com>
2025-01-31 13:57:28 +0530

    in bankExample added the base class

--- bankExample/bankExample.java
+++ bankExample/bankExample.java
@@ -1,4 +1,28 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
+class bankExample{
+	static class BankAccount{
+		int accountNumber;
+		double balance;
+		String accountHolderName;
+		void deposit(double amount){
+			balance = balance + amount;
+		}
+		boolean withdraw(double amount){
+			if(balance - amount > 0){
+				balance = balance - amount;
+				return true;
+			}
+			return false;
+		}
+		void displayAccountInfo(){
+			System.out.println("Name: " + accountHolderName);
+			System.out.println("Account number: "+ accountNumber);
+			System.out.println("Balance: " + balance);
+		}
+	}
+	public static void main(String[] args){
+	
+	}
+}
